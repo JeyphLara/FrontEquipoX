8865f174f9f3876e426199c7a52aefcf037e168a
JeyphLara <jfelipelara@estudiante.uniajc.edu.co>
2025-04-27 23:24:25 -0500

    formulario actualizado

--- src/api/laboratorioApi.ts
+++ src/api/laboratorioApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const laboratorioApi = axios.create({
-  baseURL: 'http://ec2-18-117-154-8.us-east-2.compute.amazonaws.com:60001/api/v1',
+  baseURL: 'http://ec2-18-117-154-8.us-east-2.compute.amazonaws.com:60001/api/v1', 
 });
 
 export default laboratorioApi;
